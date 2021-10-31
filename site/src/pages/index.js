b03ae2d9b38d98aad343553bf263428eac081212
Chris Carrick <christophercarrick7@gmail.com>
2021-10-31 03:40:54 -0400

    Just use the sitting at desk hero for everything. It is the coolest looking hero.

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import { random } from 'lodash';
 import styled from 'styled-components';
 
 import { Layout } from '@layout';
 
 import DeskSVG from '@assets/svg/hero/desk.svg';
-import SittingSVG from '@assets/svg/hero/sitting.svg';
-import StandingSVG from '@assets/svg/hero/standing.svg';
 
 const IndexWrapper = styled.div`
   display: flex;
@@ -17,13 +14,10 @@ const IndexWrapper = styled.div`
 `;
 
 export default function IndexPage() {
-  const heros = [DeskSVG, SittingSVG, StandingSVG];
-  const Hero = heros[random(0, heros.length - 1)];
-
   return (
     <Layout>
       <IndexWrapper>
-        <Hero style={{ width: '100%', height: 'auto' }} />
+        <DeskSVG style={{ width: '100%', height: 'auto' }} />
       </IndexWrapper>
     </Layout>
   );
